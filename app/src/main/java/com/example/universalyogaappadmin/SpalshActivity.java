15c4c6c03b5f69e92c6b45cf7741b2d91a8da7bb
Vijay Jayadev <vj2758e@gre.ac.uk>
2023-11-21 11:29:01 +0000

    test

---
+++ app/src/main/java/com/example/universalyogaappadmin/SpalshActivity.java
@@ -0,0 +1,24 @@
+package com.example.universalyogaappadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SpalshActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
