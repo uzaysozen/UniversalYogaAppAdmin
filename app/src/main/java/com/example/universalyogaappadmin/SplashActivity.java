e463ffd453b5aa84441a97d8b3eea735badbcbf8
Kerem Alpdag <ka6327n@gre.ac.uk>
2023-11-21 12:02:44 +0000

    splash screen all complete

--- app/src/main/java/com/example/universalyogaappadmin/SplashActivity.java
+++ app/src/main/java/com/example/universalyogaappadmin/SplashActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SpalshActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spalsh);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SpalshActivity.this, LandingPage.class);
+                Intent intent = new Intent(SplashActivity.this, LandingPage.class);
                 startActivity(intent);
                 finish();
             }
