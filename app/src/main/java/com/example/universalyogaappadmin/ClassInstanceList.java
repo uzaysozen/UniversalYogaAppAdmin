4bd1b73458b7ec6797fb50503dcb2491c86d3874
Kerem Alpdag <ka6327n@gre.ac.uk>
2023-11-30 18:59:22 +0000

    tested webkit

--- app/src/main/java/com/example/universalyogaappadmin/ClassInstanceList.java
+++ app/src/main/java/com/example/universalyogaappadmin/ClassInstanceList.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,23 +16,43 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class ClassInstanceList extends AppCompatActivity {
 
     private ListView lv;
     private DatabaseHelper dbHelper;
+    private WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dbHelper = new DatabaseHelper(this);
+        String result = dbHelper.getWebFormat().toString();
         setContentView(R.layout.class_instance_list);
         //Toolbar appToolbar = (Toolbar) findViewById(R.id.toolbar_top);
         //setSupportActionBar(appToolbar);
-        JSONArray result = dbHelper.getCourseDetails();
+        JSONArray result_uzay = dbHelper.getCourseDetails();
+        browser = (WebView) findViewById(R.id.webkit);
 
         lv = (ListView) findViewById(R.id.instanceListView);
 
-        ClassAdapter classAdapter = new ClassAdapter(getApplicationContext(), result);
+        ClassAdapter classAdapter = new ClassAdapter(getApplicationContext(), result_uzay);
         lv.setAdapter(classAdapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -50,7 +71,140 @@ public class ClassInstanceList extends AppCompatActivity {
 
         FloatingActionButton upload = findViewById(R.id.uploadFloatingBtn);
         upload.setOnClickListener(v -> {
-            Toast.makeText(this, "Class has been created with id:" , Toast.LENGTH_SHORT).show();
+            try {
+                URL pageURL = new URL(getString(R.string.url));
+                trustAllHosts();
+                HttpURLConnection con = (HttpURLConnection) pageURL.openConnection();
+
+                //String jsonString = getString(R.string.json);
+
+                JsonThread myTask = new JsonThread(this, con, result);
+                System.out.println("anan" + result);
+                Thread t1 = new Thread(myTask, "JSON Thread");
+                t1.start();
+                //Toast.makeText(this, "debug", Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
+    }
+
+    private void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[] {};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+        } };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class JsonThread implements Runnable
+    {
+        private AppCompatActivity activity;
+        private HttpURLConnection con;
+        private String jsonPayLoad;
+
+        public JsonThread(AppCompatActivity activity, HttpURLConnection con, String jsonPayload)
+        {
+            this.activity = activity;
+            this.con = con;
+            this.jsonPayLoad = jsonPayload;
+        }
+
+        @Override
+        public void run()
+        {
+            String response = "";
+            if (prepareConnection()) {
+                response = postJson();
+            } else {
+                response = "Error preparing the connection";
+            }
+            showResult(response);
+        }
+
+
+        private void showResult(String response) {
+            activity.runOnUiThread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    String page = generatePage(response);
+                    browser.loadData(page, "text/html", "UTF-8");
+                }
+            });
+        }
+
+        private String postJson() {
+            String response = "";
+            try {
+                String postParameters = "jsonpayload=" + URLEncoder.encode(jsonPayLoad, "UTF-8");
+                con.setFixedLengthStreamingMode(postParameters.getBytes().length);
+                PrintWriter out = new PrintWriter(con.getOutputStream());
+                out.print(postParameters);
+                out.close();
+                int responseCode = con.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    response = readStream(con.getInputStream());
+                    System.out.println("Selamin Aleykum" + response);
+                } else {
+                    response = "Error contacting server: " + responseCode;
+                }
+            } catch (Exception e) {
+                response = e.toString();//"Error executing code";
+                System.out.println("Selamin Aleykum" + response);
+            }
+            return response;
+        }
+
+        private String readStream(InputStream in) {
+            StringBuilder sb = new StringBuilder();
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                String nextLine = "";
+                while ((nextLine = reader.readLine()) != null) {
+                    sb.append(nextLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+
+        private String generatePage(String content) {
+            return "<html><body><p>" + content + "</p></body></html>";
+        }
+
+
+        private boolean prepareConnection() {
+            try {
+                con.setDoOutput(true);
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                return true;
+
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/universalyogaappadmin/MainActivity.java
+++ app/src/main/java/com/example/universalyogaappadmin/MainActivity.java
@@ -148,17 +148,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private WebView browser;
-
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new DatabaseHelper(this);
-        browser = (WebView) findViewById(R.id.webkit);
         //String result = getResources().getString(R.string.json);
-        String result = dbHelper.getWebFormat().toString();
 
         Intent intent = getIntent();
         courseId = intent.getIntExtra("courseId", -1);
@@ -195,7 +191,6 @@ public class MainActivity extends AppCompatActivity {
         Button create = (Button) findViewById(R.id.CreateSessionButton);
         Toolbar appToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(appToolbar);
-        String escapedJson = result;
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -204,50 +199,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        try {
-            URL pageURL = new URL(getString(R.string.url));
-            trustAllHosts();
-            HttpURLConnection con = (HttpURLConnection) pageURL.openConnection();
-
-            //String jsonString = getString(R.string.json);
-
-            JsonThread myTask = new JsonThread(this, con, escapedJson);
-            System.out.println("anan" + escapedJson);
-            Thread t1 = new Thread(myTask, "JSON Thread");
-            t1.start();
-            //Toast.makeText(this, "debug", Toast.LENGTH_SHORT).show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
-
-    private void trustAllHosts() {
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new java.security.cert.X509Certificate[] {};
-            }
-
-            public void checkClientTrusted(X509Certificate[] chain,
-                                           String authType) throws CertificateException {
-            }
-
-            public void checkServerTrusted(X509Certificate[] chain,
-                                           String authType) throws CertificateException {
-            }
-        } };
-
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection
-                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -263,97 +215,5 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    class JsonThread implements Runnable
-    {
-        private AppCompatActivity activity;
-        private HttpURLConnection con;
-        private String jsonPayLoad;
-
-        public JsonThread(AppCompatActivity activity, HttpURLConnection con, String jsonPayload)
-        {
-            this.activity = activity;
-            this.con = con;
-            this.jsonPayLoad = jsonPayload;
-        }
-
-        @Override
-        public void run()
-        {
-            String response = "";
-            if (prepareConnection()) {
-                response = postJson();
-            } else {
-                response = "Error preparing the connection";
-            }
-            showResult(response);
-        }
-
-
-        private void showResult(String response) {
-            activity.runOnUiThread(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    String page = generatePage(response);
-                    ((MainActivity)activity).browser.loadData(page, "text/html", "UTF-8");
-                }
-            });
-        }
-
-        private String postJson() {
-            String response = "";
-            try {
-                String postParameters = "jsonpayload=" + URLEncoder.encode(jsonPayLoad, "UTF-8");
-                con.setFixedLengthStreamingMode(postParameters.getBytes().length);
-                PrintWriter out = new PrintWriter(con.getOutputStream());
-                out.print(postParameters);
-                out.close();
-                int responseCode = con.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    response = readStream(con.getInputStream());
-                    System.out.println("Selamin Aleykum" + response);
-                } else {
-                    response = "Error contacting server: " + responseCode;
-                }
-            } catch (Exception e) {
-                response = e.toString();//"Error executing code";
-                System.out.println("Selamin Aleykum" + response);
-            }
-            return response;
-        }
-
-        private String readStream(InputStream in) {
-            StringBuilder sb = new StringBuilder();
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-                String nextLine = "";
-                while ((nextLine = reader.readLine()) != null) {
-                    sb.append(nextLine);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return sb.toString();
-        }
-
-        private String generatePage(String content) {
-            return "<html><body><p>" + content + "</p></body></html>";
-        }
-
-
-        private boolean prepareConnection() {
-            try {
-                con.setDoOutput(true);
-                con.setRequestMethod("POST");
-                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                return true;
-
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            }
-            return false;
-        }
     }
 }
